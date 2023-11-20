85f1dbae882594f1e03f05a674816ea51b26aa20
Ankitsreenivasa <ankit.sreeni@gmail.com>
2023-11-20 22:24:18 +0530

    Arrays

--- Arrays/Java03.java
+++ Arrays/Java03.java
@@ -2,10 +2,17 @@ package Arrays;
 
 import java.util.*;
 
+//Swapping Elements
 public class Java03 {
     public static void main(String[] args) {
-        int[] arr = { 12, 434, 46, 76, 8, 88 };
+        int[] arr = { 12, 434, 46, 76, 8, 88, 112 };
+        // Swapping Elements
+        System.out.println("---Swapping Elements---");
         swap(arr, 3, 0);
+        System.out.println(Arrays.toString(arr));
+
+        System.out.println("---Reversing Array---");
+        reverse(arr);
         System.out.println(Arrays.toString(arr));
     }
 
@@ -13,5 +20,17 @@ public class Java03 {
         int temp = arr[index1];
         arr[index1] = arr[index2];
         arr[index2] = temp;
+    }
+
+    static void reverse(int[] arr) {
+
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start < end) {
+            swap(arr, start, end);
+            start++;
+            end--;
+        }
     }
 }
---
+++ Arrays/Java04.java
@@ -0,0 +1,44 @@
+package Arrays;
+
+public class Java04 {
+    public static void main(String[] args) {
+        int[] arr = { 10, 43, 46, 100, 8, 88 };
+
+        System.out.println("Max Val : " + max(arr));
+        System.out.println("Max Val in Range : " + maxInRange(arr, 1, 2));
+    }
+
+    static int max(int[] arr) {
+
+        if (arr.length == 0) {
+            return -1;
+        }
+
+        int max = arr[0];
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+
+        return max;
+    }
+
+    static int maxInRange(int[] arr, int ind1, int ind2) {
+
+        if (arr.length == 0) {
+            return -1;
+        }
+
+        int max = arr[0];
+
+        for (int i = ind1; i <= ind2; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+
+        return max;
+    }
+}
