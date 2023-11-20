4746643837de54c6627753091ad5bd862aec7b16
Ankitsreenivasa <ankit.sreeni@gmail.com>
2023-11-20 19:11:08 +0530

    Arrays

--- Arrays/Java02.java
+++ Arrays/Java02.java
@@ -1,4 +1,5 @@
 package Arrays;
+
 import java.util.*;
 // import java.util.ArrayList;
 
@@ -6,25 +7,41 @@ public class Java02 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        //Creating Array List
-        ArrayList<String> inpuArrayList = new ArrayList<>();
+        // Creating Array List
+        ArrayList<Integer> list = new ArrayList<>();
+        ArrayList<Integer> list1 = new ArrayList<>();
 
-        System.out.println("Enter the elements :");
-        System.out.println("type 'done' after giving the input");
-        while(true){
-            String inp=sc.nextLine();
+        // System.out.println("Enter the elements :");
+        // System.out.println("type 'done' after giving the input");
 
-            if(inp.equalsIgnoreCase("done")){
-                break;
-            }
+        list.add(12);
+        list.add(32);
+        list.add(14);
+        list.add(3);
+        list.add(2);
+        list.add(30);
+        list.add(13);
+        list.add(35);
 
-            inpuArrayList.add(inp);
+        System.out.println(list.contains(5));
+        list.set(0, 240);
+        System.out.println(list);
+        list.remove(2);
+        System.out.println(list);
+        list.remove(Integer.valueOf(35));
+        System.out.println(list);
+
+        System.out.println("--------------");
+        System.out.println("Enter elements : ");
+        for (int i = 0; i < 5; i++) {
+            list1.add(sc.nextInt());
+        }
+        System.out.println(list1);
+        System.out.println("--OR--");
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println(list1.get(i));
         }
 
-        for (String string : inpuArrayList) {
-            System.out.println(string);
-        }
-
-        
     }
 }
---
+++ Arrays/Java03.java
@@ -0,0 +1,16 @@
+package Arrays;
+
+import java.util.*;
+
+public class Java03 {
+    public static void main(String[] args) {
+        int[] arr = { 12, 434, 46, 76, 8, 88 };
+        swap(arr, 3, 0);
+    }
+
+    static void swap(int[] arr, int index1, int index2) {
+        int temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
+    }
+}
