233b6fc054bedccf21165b311616f3fe90ed407a
Ankitsreenivasa <ankit.sreeni@gmail.com>
2023-11-21 09:11:30 +0530

    Linear Search

--- LinearSearch/SearchInStrings.java
+++ LinearSearch/SearchInStrings.java
@@ -2,9 +2,13 @@ package LinearSearch;
 
 public class SearchInStrings {
     public static void main(String[] args) {
-        String str = "Ankit";
-        char key = 'i';
+        String str = "Fortunex";
+        char key = 'x';
         System.out.println(stringSearch(str, key));
+
+        // In Range
+        System.out.println("---Search in Range---");
+        System.out.println(stringSearchRange(str, key));
 
     }
 
@@ -12,12 +16,30 @@ public class SearchInStrings {
         if (str.length() == 0) {
             return false;
         }
-
         for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i)==key) {
-                System.out.println("Key Found at "+ i);
+            if (str.charAt(i) == key) {
+                System.out.println("Key Found at " + i);
                 return true;
-                
+
+            }
+        }
+
+        return false;
+    }
+
+    static boolean stringSearchRange(String str, char key) {
+        if (str.length() == 0) {
+            return false;
+        }
+
+        int start = 1;
+        int end = 7;
+
+        for (int i = start; i <= end; i++) {
+            if (str.charAt(i) == key) {
+                System.out.println("Key Found at " + i);
+                return true;
+
             }
         }
 
