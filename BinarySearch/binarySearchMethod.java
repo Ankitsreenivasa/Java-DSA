138ffdbe01d60922707dda15daadedaf61e40e8f
Ankitsreenivasa <ankit.sreeni@gmail.com>
2023-11-25 19:24:47 +0530

    Binary Search

---
+++ BinarySearch/binarySearchMethod.java
@@ -0,0 +1,22 @@
+package BinarySearch;
+
+import java.util.Arrays;
+
+public class binarySearchMethod {
+    public static void main(String[] args) {
+        // UnSorted Array
+        int[] arr = { 56, 66, 11, 12, 13, 10, 45, 23, 34, };
+        System.out.println("Unsorted Array : " + Arrays.toString(arr));
+        // Sorting the Array
+        Arrays.sort(arr);
+
+        String nums = Arrays.toString(arr);
+        System.out.println("Sorted Array : " + nums);
+
+        int key = 45;
+
+        int index = Arrays.binarySearch(arr, key);
+
+        System.out.println("Element found at index " + index);
+    }
+}
