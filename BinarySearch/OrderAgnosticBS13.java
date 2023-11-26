4ff5efc93b7adbe23b7e1e3846a26cf88efbcbac
Ankitsreenivasa <ankit.sreeni@gmail.com>
2023-11-26 17:57:33 +0530

    binary search

--- BinarySearch/OrderAgnosticBS13.java
+++ BinarySearch/OrderAgnosticBS13.java
@@ -2,6 +2,65 @@ package BinarySearch;
 
 public class OrderAgnosticBS13 {
     public static void main(String[] args) {
-        int arr[] = {90,80,70,60,50,40,30,25,20,15,10};
+        int arr[] = {9,8,7,6,5,4,3,2,1};
+
+        int key=7;
+
+        orderAgnosticBinarySearch(arr, key);
     }
+
+    static void orderAgnosticBinarySearch(int arr[], int key){
+        if (arr[0]<arr[arr.length-1]) {
+            System.out.println(ascOrder(arr, key));
+        }
+        else{
+            System.out.println(descOrder(arr,key));
+        }
+        
+        
+    }
+
+    private static int ascOrder(int arr[], int key) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+            // find the mid element
+            int mid = start + (end - start) / 2; // OR (start + end) / 2
+            if (key < arr[mid]) {
+                end = mid - 1;
+            } else if (key > arr[mid]) {
+                start = mid + 1;
+            } else {
+                return mid;
+            }
+
+        }
+
+        return -1;
+
+    }
+
+    private static int descOrder(int[] arr, int key) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+            // find the mid element
+            int mid = start + (end - start) / 2; // OR (start + end) / 2
+            if (key > arr[mid]) {
+                end = mid - 1;
+            } else if (key < arr[mid]) {
+                start = mid + 1;
+            } else {
+                return mid;
+            }
+
+        }
+
+        return -1;
+
+    }
+
+    
 }
